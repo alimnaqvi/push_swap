26ce1ea4e3205240c322f7aa5a007f0adc0b0d95
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2024-11-10 17:19:56 +0100

    add list util functions

--- utils_list.c
+++ utils_list.c
@@ -6,28 +6,64 @@
 /*   By: anaqvi <anaqvi@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 14:56:03 by anaqvi            #+#    #+#             */
-/*   Updated: 2024/11/10 13:24:57 by anaqvi           ###   ########.fr       */
+/*   Updated: 2024/11/10 17:15:45 by anaqvi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_sorted(t_list *lst)
-{
-	//
-}
-
 t_list	*ft_lstnew(int num)
 {
-	//
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!(new_node))
+		return (NULL);
+	new_node->num = num;
+	new_node->next = NULL;
+	return (new_node);
 }
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	//
+	t_list	*last;
+
+	if (!lst)
+		return ;
+	last = *lst;
+	while (last->next != NULL)
+		last = last->next;
+	if (!last)
+		*lst = new;
+	else
+		last->next = new;
 }
 
 int	num_is_in_list(t_list *lst, int num)
 {
-	//
+	t_list	*current;
+
+	current = lst;
+	while (current)
+	{
+		if (current->num == num)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
+
+int	is_sorted(t_list *lst)
+{
+	t_list	*current;
+	
+	if (current->next == NULL)
+		return (1);
+	while (current->next)
+	{
+		if (current->num > current->next->num)
+			return (0);
+		current = current->next;
+	}
+	return (1);
 }
