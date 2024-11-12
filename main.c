110893e6067f684a8ac15a727de6fe8709ef40ad
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2024-11-12 20:00:18 +0100

    add ideas for next sorting algorithm as pseudo-code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anaqvi <anaqvi@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 12:14:26 by anaqvi            #+#    #+#             */
-/*   Updated: 2024/11/12 19:32:42 by anaqvi           ###   ########.fr       */
+/*   Updated: 2024/11/12 19:37:22 by anaqvi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,29 +37,29 @@ static void	ft_exit(int exit_status, t_list **stack_a, t_list **stack_b)
 	exit(EXIT_FAILURE);
 }
 
-#include <stdio.h>
-void check_print_list_exit(t_list *stack_a, t_list *stack_b)
-{
-	t_list *current = stack_a;
-	printf("Numbers saved in the stack a:\n");
-	while (current)
-	{
-		printf("%i  ", current->num);
-		current = current->next;
-	}
-	printf("\n");
+// #include <stdio.h>
+// void check_print_list_exit(t_list *stack_a, t_list *stack_b)
+// {
+// 	t_list *current = stack_a;
+// 	printf("Numbers saved in the stack a:\n");
+// 	while (current)
+// 	{
+// 		printf("%i  ", current->num);
+// 		current = current->next;
+// 	}
+// 	printf("\n");
 
-	current = stack_b;
-	printf("Numbers saved in the stack b:\n");
-	while (current)
-	{
-		printf("%i  ", current->num);
-		current = current->next;
-	}
-	printf("\n");
+// 	current = stack_b;
+// 	printf("Numbers saved in the stack b:\n");
+// 	while (current)
+// 	{
+// 		printf("%i  ", current->num);
+// 		current = current->next;
+// 	}
+// 	printf("\n");
 
-	ft_exit(0, &stack_a, &stack_b);
-}
+// 	ft_exit(0, &stack_a, &stack_b);
+// }
 
 int	main(int argc, char **argv)
 {
@@ -76,7 +76,7 @@ int	main(int argc, char **argv)
 		ft_exit(0, &stack_a, &stack_b);
 	if (simplify_data(stack_a) == -1)
 		ft_exit(1, &stack_a, &stack_b);
-	check_print_list_exit(stack_a, stack_b); // delete
+	// check_print_list_exit(stack_a, stack_b); // delete
 	if (sort_and_display_ops(&stack_a, &stack_b) == -1) // change to void if always returns 0
 		ft_exit(1, &stack_a, &stack_b);
 	ft_exit(0, &stack_a, &stack_b);
--- sort_and_display_ops.c
+++ sort_and_display_ops.c
@@ -6,15 +6,29 @@
 /*   By: anaqvi <anaqvi@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 14:56:25 by anaqvi            #+#    #+#             */
-/*   Updated: 2024/11/12 19:11:25 by anaqvi           ###   ########.fr       */
+/*   Updated: 2024/11/12 19:59:05 by anaqvi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// Find smallest in stack a then push to stack b
+// mallocs a new list in which to save the operations
+// at the end, optimizes the operations then frees the the malloced list
 int	sort_and_display_ops(t_list **stack_a, t_list **stack_b)
 {
-	//
+	// while (stack_a || stack_a is unsorted)
+		// Find smallest number (min_stack_a) in stack a
+		// If min_stack_a is in the first half, ra until it is on top
+		// else if min_stack_a is in the second half, rra until it is on top
+		// then push_b
+	// while (stack_b)
+		// push_a
+	// call function to optimize the list of steps saved in a list
+		// replace with ss, rr, rrr when necessary
+		// remove neutral moves (e.g., pa + pb; sa + sa; ra + rra)
+	// display the optimized list of operations
+	// free the list of operations
 }
 
 // Initial bubble sort
